20b3905014a058c185dee643c6146d976cdfe978
Sourodeep Sarkar <sourodeep.sarkar.dbpc@gmail.com>
2026-07-19 19:54:42 +0530

    Added Valid API Keys

--- public/js/firebase.js
+++ public/js/firebase.js
@@ -6,12 +6,13 @@ import {
 } from "https://www.gstatic.com/firebasejs/12.0.0/firebase-auth.js";
 
 const firebaseConfig = {
-    apiKey: "...",
-    authDomain: "...",
-    projectId: "...",
-    storageBucket: "...",
-    messagingSenderId: "...",
-    appId: "..."
+    apiKey: "AIzaSyDXGucVOsaCKsL_KuYrUBYjIH5yBvYqt3I",
+    authDomain: "solace-21d29.firebaseapp.com",
+    projectId: "solace-21d29",
+    storageBucket: "solace-21d29.firebasestorage.app",
+    messagingSenderId: "589692348041",
+    appId: "1:589692348041:web:c1fc3ed7c40c6a57e3c858",
+    measurementId: "G-1DMT95DSFF"
 };
 
 const app = initializeApp(firebaseConfig);
